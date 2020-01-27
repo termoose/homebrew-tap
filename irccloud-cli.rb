a99896561174104aaaa3c5cfe1e9cb7cedcbee86
goreleaserbot <goreleaser@carlosbecker.com>
2020-01-27 19:58:30 +0100

    Brew formula update for irccloud version v0.1.3

--- irccloud-cli.rb
+++ irccloud-cli.rb
@@ -2,16 +2,16 @@
 class IrccloudCli < Formula
   desc "Text-mode terminal client for IRCCloud"
   homepage "https://github.com/termoose/irccloud"
-  version "0.1.2"
+  version "0.1.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/termoose/irccloud/releases/download/v0.1.2/irccloud_0.1.2_Darwin_x86_64.tar.gz"
-    sha256 "cc19a3d7bfc9fd541b65975f6e3429d377ca582b370a67515a3d54ab36fe4c32"
+    url "https://github.com/termoose/irccloud/releases/download/v0.1.3/irccloud_0.1.3_Darwin_x86_64.tar.gz"
+    sha256 "be4b949ce771b81ef743979707d15595f208e9090170b002b00629fe604f706e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/termoose/irccloud/releases/download/v0.1.2/irccloud_0.1.2_Linux_x86_64.tar.gz"
-      sha256 "f17b547d161b40feb4a079106d35e5c92c743b98589af77f63f392f3a7ba7dd1"
+      url "https://github.com/termoose/irccloud/releases/download/v0.1.3/irccloud_0.1.3_Linux_x86_64.tar.gz"
+      sha256 "3f342ee266f768c9dadc272916602e29f73857245e50f53b19c139fb2c50b5bb"
     end
   end
 
