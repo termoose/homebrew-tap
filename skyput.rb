e623fb8f99c96ca930f6aff884ff4f09bba85299
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-09 22:45:05 +0200

    Brew formula update for skyput version v0.2.0

--- skyput.rb
+++ skyput.rb
@@ -2,16 +2,16 @@
 class Skyput < Formula
   desc "File upload utility for Skynet"
   homepage "https://github.com/termoose/skyput"
-  version "0.1.0"
+  version "0.2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/termoose/skyput/releases/download/v0.1.0/skyput_0.1.0_Darwin_x86_64.tar.gz"
-    sha256 "da0e0f661d5537e2edca3a255f7c485efc108cec3739247f2c2789b77ada04f5"
+    url "https://github.com/termoose/skyput/releases/download/v0.2.0/skyput_0.2.0_Darwin_x86_64.tar.gz"
+    sha256 "b02bb62dc34eb54cb4997ecea922ea30a5b3fa19b302f3e535aa4aab221fd63f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/termoose/skyput/releases/download/v0.1.0/skyput_0.1.0_Linux_x86_64.tar.gz"
-      sha256 "0b6ed8a6bf2bc857048b6d1237afd8460331d80bf4931fb6b442a532d90eda26"
+      url "https://github.com/termoose/skyput/releases/download/v0.2.0/skyput_0.2.0_Linux_x86_64.tar.gz"
+      sha256 "e64d00a6c6d0cd584771ce77c0d5f3fe585ae77ba643fa9dc55280ccd8e2fdb1"
     end
   end
 
